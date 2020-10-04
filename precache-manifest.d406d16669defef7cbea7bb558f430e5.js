b415e5da76ab12ff309593e1d4bfd1b676614f61
Mohamed Saleh <mo01144368997>
2020-10-04 19:22:42 +0200

    Updates

---
+++ precache-manifest.d406d16669defef7cbea7bb558f430e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf677a2a55c263a19c4b7065688e41e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2eee4e25bd067fb5f431",
+    "url": "/static/css/2.bdc6a3fc.chunk.css"
+  },
+  {
+    "revision": "e9a60368714ed2a81808",
+    "url": "/static/css/main.d0fcbf84.chunk.css"
+  },
+  {
+    "revision": "2eee4e25bd067fb5f431",
+    "url": "/static/js/2.14443b2f.chunk.js"
+  },
+  {
+    "revision": "c3cb654a3a7d27a576aed8d503b856d8",
+    "url": "/static/js/2.14443b2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9a60368714ed2a81808",
+    "url": "/static/js/main.3f3dfa7e.chunk.js"
+  },
+  {
+    "revision": "44336a08bec6373a09ba",
+    "url": "/static/js/runtime-main.80850ae3.js"
+  },
+  {
+    "revision": "c23e8fde6a132231d7d8830092613ee4",
+    "url": "/static/media/1.c23e8fde.png"
+  }
+]);
